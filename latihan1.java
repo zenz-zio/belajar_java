c0f246cdf8f10282947d416d98c8b2b1c1f75b6a
zenz-zio <zavirozio030@gmail.com>
2025-05-21 14:22:20 +0700

    Add files via upload

---
+++ latihan1.java
@@ -0,0 +1,16 @@
+
+public class latihan1 {
+
+    static void main(String[] args) {
+        String nama = "jamal";
+        String kelas = "X PPLG 2";
+        double B_badan = 50.00;
+
+        System.out.println("Nama Saya : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.print("Berat Badan : ");
+        System.out.println(B_badan);
+
+    }
+
+}
