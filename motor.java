f14e154012d05d6a93517865067fbd72b637f731
zenz-zio <zavirozio030@gmail.com>
2025-05-21 14:20:21 +0700

    Add files via upload

---
+++ motor.java
@@ -0,0 +1,17 @@
+public class motor {
+    String warna = "pink";
+
+    public void jalankan() {
+        System.out.println("masukkan gigi 1 , lepaskan kopling sambil tekan gas");
+
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public String getWarana(String warna) {
+        return this.warna;
+    }
+
+}
