ac1d32aa08d6085d14cb9571298f5e13c15e2ae9
zenz-zio <zavirozio030@gmail.com>
2025-05-21 14:22:33 +0700

    Add files via upload

---
+++ latiahn2.java
@@ -0,0 +1,18 @@
+
+public class latiahn2 {
+    // Menggunakan operator aritmatika
+    public static void main(String[] args) {
+        int a = 10;
+
+        int b = 20;
+        int c = 25;
+        int d = 30;
+        System.out.println("Hasil a + b" + (a + b));
+        System.out.println("Hasil a - b" + (a - b));
+        System.out.println("Hasil a * b" + (a * b));
+        System.out.println("Hasil b / a" + (b / a));
+        System.out.println("Hasil c % a" + (c % a));
+        System.out.println("Hasil d % c" + (d % c));
+
+    }
+}
